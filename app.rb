77434bd924d59ff6dd598e818697175202564555
Your Name <josefdaly@gmail.com>
2015-08-26 22:50:59 -0700

    Integrate database with sinatra app

--- app.rb
+++ app.rb
@@ -1,15 +1,30 @@
 require 'sinatra'
+require 'json'
 require_relative './active-record/db_connection'
 require_relative './active-record/sql_object'
+
+Tilt.register Tilt::ERBTemplate, 'html.erb'
 
 class Url < SQLObject
   self.finalize!
 end
 
+Url.new({ url: 'https://www.google.com', shortened: '1' }).save
+Url.new({ url: 'https://www.example.com', shortened: '2' }).save
+
 get '/urls' do
-  @urls = Url.all
+  content_type :json
+  urls_array = []
+  urls = Url.all
+  urls.each do |url_model|
+    urls_array.push(url_model.attributes)
+  end
+
+  
 end
 
-get '/:path' do 
-  if Url.where(params[:path]).size > 0
-    redirect
+#
+# get '/:path' do
+#   url =
+#   if Url.where(params[:path]).size > 0
+#     redirect
