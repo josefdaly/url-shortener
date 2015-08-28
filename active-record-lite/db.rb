3d9a6abb9fa88a96081e648a25c2f25d381ec43d
Your Name <josefdaly@gmail.com>
2015-08-27 20:49:45 -0700

    Set db options

--- active-record-lite/db.rb
+++ active-record-lite/db.rb
@@ -3,14 +3,17 @@ require 'pg'
 class Database < PG::Connection
 
   def initialize
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
     # Database information retrieved from Heroku PostrgreSQL add-on
-    # super(
-    #   host: # Your db info,
-    #   dbname: # Your db info,
-    #   port: # Your db info,
-    #   password: # Your db info,
-    #   user: # Your db info
-    # )
+    super(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
   end
 
   def self.exec(*args)
