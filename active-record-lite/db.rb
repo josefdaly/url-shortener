1b409ead2c84fbef01e9caa899b21e78d15dd37c
Your Name <josefdaly@gmail.com>
2015-08-27 20:29:05 -0700

    Tweak to db

--- active-record-lite/db.rb
+++ active-record-lite/db.rb
@@ -4,13 +4,13 @@ class Database < PG::Connection
 
   def initialize
     # Database information retrieved from Heroku PostrgreSQL add-on
-    super(
-      host: # Your db info,
-      dbname: # Your db info,
-      port: # Your db info,
-      password: # Your db info,
-      user: # Your db info
-    )
+    # super(
+    #   host: # Your db info,
+    #   dbname: # Your db info,
+    #   port: # Your db info,
+    #   password: # Your db info,
+    #   user: # Your db info
+    # )
   end
 
   def self.exec(*args)
